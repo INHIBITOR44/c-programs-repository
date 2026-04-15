ca4d9afa398c48c05dc57025d2152ae53ac7858c
INHIBITOR44 <kabeerr007@proton.me>
2026-04-15 14:31:29 +0530

    c repo
    pointers

--- CH6/01_pointer.c
+++ CH6/01_pointer.c
@@ -2,7 +2,13 @@
 
 int main(){
     
+    int i=99;
+    int* j=&i;//ek pointer hai jo i ko batara 
+    int  k;
 
-    
+    printf("i ke address ki value hai %p\n",&i);
+    printf("j ke address ki value hai %p\n",j);
+    printf("k ki baat karte hai %d", *(&i));
+
     return 0;
 }
