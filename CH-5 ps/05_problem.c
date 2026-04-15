23ad3b11e0d5939b2eb278010484af7197d2a2b4
INHIBITOR44 <kabeerr007@proton.me>
2026-04-15 14:08:24 +0530

    c repo
    major changes

---
+++ CH-5 ps/05_problem.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int j, n;
+
+    printf("n ki value bata:\n", n);
+    scanf("%d", &n);
+
+    for (int i = 0; i <= n; i++)
+    {
+
+        for (int j = 0; j < i + 1; j++)
+        {
+
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
